d318b5ad8124ba2fe3355aa0c3b32128ec703c1d
hardik7863 <hardikbatwal@gmail.com>
2025-01-20 09:55:49 +0530

    backing up

---
+++ typescript/index.js
@@ -0,0 +1,4 @@
+// js is dynamic typed  is the main problem 
+//ts is statically typed language
+var greeting = "Hello World";
+console.log(greeting);
---
+++ typescript/index.ts
@@ -0,0 +1,5 @@
+// js is dynamic typed  is the main problem 
+//ts is statically typed language
+
+let  greeting:string="Hello World";
+console.log(greeting);
